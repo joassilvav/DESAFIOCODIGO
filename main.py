f97076000405dfa68347728c90d8ed96b8de34bf
Joas Silva <joassantos4455@gmail.com>
2023-07-05 20:20:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import dasafio1, desafio2
+import dasafio1, desafio2, desafio4
 
 print("Desafio 1- manipulação de string. Revertendo a ordem das palavras. \nDesafio 2 - Removendo duplicados. \n" )
 escolha = int(input('Escolha o numero do desafio: '))
@@ -12,3 +12,5 @@ match escolha:
 
     case 3:
         print('')
+    case 4:
+        desafio4.desafiofour()
