49e5806b44086e81b3fc3c2ea6554d51e02c5d51
Joas Silva <joassantos4455@gmail.com>
2023-07-05 17:45:09 -0300

    Update desafio2.py

--- desafio2.py
+++ desafio2.py
@@ -1,6 +1,8 @@
 from collections import OrderedDict
 
+#criando a função
 def remov(frase):
+     # transformando em dicionario e lista para usar o .join(junta lista dentro de uma string, nessa caso é o '')
      return ''.join(list(OrderedDict.fromkeys(frase)))
 
 def desafioo2():
