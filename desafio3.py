ad2aaff211900ccdb4baf260c3b72d7d7f6937cb
Joas Silva <joassantos4455@gmail.com>
2023-07-05 20:32:57 -0300

    Update desafio3.py

--- desafio3.py
+++ desafio3.py
@@ -1,3 +1,15 @@
-str(input('digite um palindromo: '))
+palav = str(input('digite um palindromo: '))
+palavinv = palav[::-1]
 
-for()
+cc = ''
+
+for i in range(len(palav)):
+    for j in range(len(palav)):
+        if j >= i:
+            string = palavinv[i:j+1]
+            if string in palav:
+                if len(string) >= len(cc):
+                    cc = string
+
+
+print(cc)
