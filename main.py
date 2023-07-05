e4460be74de6bb17a461b06feebe3c119628e23b
Joas Silva <joassantos4455@gmail.com>
2023-07-05 20:50:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import dasafio1, desafio2, desafio4
+import desafio3
+import desafio5
 
-print("Desafio 1- manipulação de string. Revertendo a ordem das palavras. \nDesafio 2 - Removendo duplicados. \n" )
+print("Desafio 1- manipulação de string. Revertendo a ordem das palavras. "
+      "\nDesafio 2 - Removendo duplicados. "
+      "\nDesafio 3- Encontrar Substring palindroma. "
+      "\nDesafio 4 - Colocar em letra maiúscula a primeira letra de cada frase. "
+      "\nDesafio 5 - Verificar se a string é um anagrama de um polímero."
+      "\n"   )
 escolha = int(input('Escolha o numero do desafio: '))
 
 match escolha:
     case 1:
         dasafio1.desafioum()
-
     case 2:
         desafio2.desafioo2()
-
     case 3:
-        print('')
+        desafio3.desafiotree()
     case 4:
         desafio4.desafiofour()
+    case 5:
+        desafio5.desafiofive()
+    case _:
+        print("Inválido")
