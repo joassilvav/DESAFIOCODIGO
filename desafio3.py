3fe20ffdad3466e44e8ceda7c8bd655b9c9d60de
Joas Silva <joassantos4455@gmail.com>
2023-07-05 20:48:49 -0300

    Update desafio3.py

--- desafio3.py
+++ desafio3.py
@@ -1,15 +1,17 @@
-palav = str(input('digite um palindromo: '))
-palavinv = palav[::-1]
+def desafiotree():
 
-cc = ''
+    palav = str(input('digite um palindromo: '))
+    palavinv = palav[::-1]
 
-for i in range(len(palav)):
-    for j in range(len(palav)):
-        if j >= i:
-            string = palavinv[i:j+1]
-            if string in palav:
-                if len(string) >= len(cc):
-                    cc = string
+    cc = ''
+
+    for i in range(len(palav)):
+        for j in range(len(palav)):
+            if j >= i:
+                string = palavinv[i:j+1]
+                if string in palav:
+                    if len(string) >= len(cc):
+                        cc = string
 
 
-print(cc)
+    print(cc)
