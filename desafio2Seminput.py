5c1ea9806e6305d19864594f01463b1ad507618d
Joas Silva <joassantos4455@gmail.com>
2023-07-07 16:11:38 -0300

    Create desafio2Seminput.py

---
+++ desafio2Seminput.py
@@ -0,0 +1,10 @@
+from collections import OrderedDict
+
+    #criando a função
+def remov(frase):
+    # transformando em dicionario e lista para usar o .join(junta lista dentro de uma string, nessa caso é o '')
+    return ''.join(list(OrderedDict.fromkeys(frase)))
+
+def desafioo2semimput():
+    texto = "Hello, World!"
+    print(remov(texto))
