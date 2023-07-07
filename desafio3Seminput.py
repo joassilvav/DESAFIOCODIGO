df5e66793ab19115427303ae248755f1b800d99c
Joas Silva <joassantos4455@gmail.com>
2023-07-07 16:16:21 -0300

    Create desafio3Seminput.py

---
+++ desafio3Seminput.py
@@ -0,0 +1,17 @@
+def desafiotreesemimput():
+
+    palav = 'babad'
+    palavinv = palav[::-1]
+
+    cc = ''
+
+    for i in range(len(palav)):
+        for j in range(len(palav)):
+            if j >= i:
+                string = palavinv[i:j+1]
+                if string in palav:
+                    if len(string) >= len(cc):
+                        cc = string
+
+
+    print(cc)
